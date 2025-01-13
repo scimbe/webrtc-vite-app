1ed01569abaa55562fc5f2c574d35310d2482a77
Martin Becke <Martin.Becke@HAW-Hamburg.de>
2025-01-13 22:04:57 +0100

    Import und Konfiguration von RoomManager korrigiert

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import { WebSocketServer } from 'ws';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import RoomManager from './RoomManager.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
